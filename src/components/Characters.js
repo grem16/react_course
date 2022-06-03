5befa17663ce379317a77bf53660fab2b390241c
Grem <eliasgarcia1012@gmail.com>
2022-06-03 17:55:07 -0400

    more changes, almost ended

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,16 +3,42 @@ export default function Characters(props) {
 
     return (
         <div className="characters">
-            <h1>Personajes</h1>
+            <h2>Personajes</h2>
             <span className="back-home">Volver</span>
             <div className="container-characters">
                 {characters.map((character, index) => (
                     <div className="character-container" key={index}>
-                        <p>{character.name}</p>
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                        <span className="alive" />
+                                        Alive
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className="dead" />
+                                        Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios:</span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Especie:</span>
+                                <span>{character.episodhhh}</span>
+                            </p>
+                        </div>
                     </div>
                 ))}
-
             </div>
+            <span className="back-home">Volver</span>
         </div>
     )
 }
